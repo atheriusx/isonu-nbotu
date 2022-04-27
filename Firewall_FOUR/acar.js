f3b439dbe0b8d8029596d744cb02fb128c7293c4
atheriusx <90158326+atheriusx@users.noreply.github.com>
2022-04-27 04:27:12 +0300

    sd
    sd

--- Firewall_FOUR/acar.js
+++
@@ -1,8 +0,0 @@
-const { acar, Mongo } = require('./Global.Client');
-const client = global.client = new acar({ fetchAllMembers: true })
-
-Mongo.Connect();
-client.fetchEvents();
-
-client.login(sistem.SECTOKENS.FOUR);
-
--- Moderation/Database/Schema/AuditRoles.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const schema = mongoose.model("AuditRole", new mongoose.Schema({
-    user: String, 
-    roller: Array
-}));
-
-module.exports = schema;
