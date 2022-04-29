acc8d4c822ecd029e8a68271f656355f20d571b9
atheriusx <90158326+atheriusx@users.noreply.github.com>
2022-04-29 03:00:22 +0300

    as
    as

--- acar.js
+++
@@ -1,9 +0,0 @@
-const { acar, Mongo } = require('./Dist.Main.Client');
-const client = global.client = new acar({ fetchAllMembers: true })
-
-client.fetchCommands()
-client.fetchEvents()
-Mongo.Connect();
-
-client.login(sistem.SECTOKENS.DISTMAIN);
-
