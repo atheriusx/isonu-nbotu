2911aba5b36d68526fcf5545fc455c8995b529a9
atheriusx <90158326+atheriusx@users.noreply.github.com>
2022-05-02 00:32:31 +0300

    s
    s

--- Moderation/Database/Schema/Voicemutes.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const schema = mongoose.model('Voicemute', new mongoose.Schema({
-    No: Number,
-    _id: String,
-    Kalkma: String
-}));
-
-module.exports = schema;
